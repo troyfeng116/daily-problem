fd3ba8a23becac92846fc321d699a6291e7a5b73
Troy Feng <troy.feng@yale.edu>
2020-08-07 13:54:22 -0400

    Solved bracket matching problem using stack structure

--- Aug052020.cpp
+++ Aug052020.cpp
@@ -30,13 +30,56 @@ typedef struct Node *Stack;
 struct Node {
 	char val;
 	Node *next;
+};
+
+void push(Stack *s, char c) {
+	Stack newNode = new Node;
+	newNode->val = c;
+	newNode->next = *s;
+	*s = newNode;
 }
 
-void insert(Stack *s, char c) {
+char pop(Stack *s) {
+	if (*s == NULL) return '#';
+	char c = (*s)->val;
+	Stack temp = *s;
+	(*s) = (*s)->next;
+	delete temp;
+	return c;
+}
 
+bool isEmpty(Stack *s) {
+	return *s == NULL;
+}
+
+bool validBrackets(string s) {
+	Stack stack = NULL;
+	for (int i = 0; i < s.length(); i++) {
+		char c = s.at(i);
+		if (c == '(' || c == '{' || c == '[') push(&stack, c);
+		else if (c == ')') {
+			if (pop(&stack) != '(') return false;
+		}
+		else if (c == '}') {
+			if (pop(&stack) != '{') return false;
+		}
+		else if (c == ']') {
+			if (pop(&stack) != '[') return false;
+		}
+		else {
+			std::cout << "INVALID INPUT STRING\n";
+			return false;
+		}
+	}
+	return isEmpty(&stack);
 }
 
 int main(int argc, char **argv) {
-	Stack stack = NULL;
+	string s1 = "((()))";
+	string s2 = "[()]{}";
+	string s3 = "({[)]";
+	std::cout << validBrackets(s1) << '\n';
+	std::cout << validBrackets(s2) << '\n';
+	std::cout << validBrackets(s3) << '\n';
 	return 0;
 }
