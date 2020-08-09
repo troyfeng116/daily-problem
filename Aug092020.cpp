a876ab62bed704d1248be4ae6552a1e766c00230
Troy Feng <troy.feng@yale.edu>
2020-08-09 10:40:25 -0400

    Created file for 8/09

---
+++ Aug092020.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+You are given a list of numbers, and a target number k. Return whether or not there are two numbers in
+the list that add up to k.
+
+Example:
+Given [4, 7, 1 , -3, 2] and k = 5,
+return true since 4 + 1 = 5.
+
+Try to do it in a single pass of the list.
+*/
+
+int main(int argc, char **argv) {
+	std::cout << "Hello\n";
+	return 0;
+}
