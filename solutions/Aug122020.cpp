d4611a3e819a8e240d1af69f7114b163dc308707
Troy Feng <troy.feng@yale.edu>
2020-08-12 12:02:33 -0400

    Solved 8/12 problem (ceiling and floor of value in BST) using modified BST search

--- solutions/Aug122020.cpp
+++ solutions/Aug122020.cpp
@@ -6,6 +6,21 @@ Given an integer k and a binary search tree, find the floor (less than or equal 
 ceiling (larger than or equal to) of k. If either does not exist, then print them as None.
 */
 
+/*
+Let's think about ceil first.
+Run normal search in BST.
+If k == node.val, return k.
+If k > node.val
+	Search right (ceil can't be node or anything in node.left)
+If k < node.val
+	If node.left == null, return node
+	Else return largest value in node.left
+
+If k is larger than any node in node.val, we will eventually hit right child of right-most node, and our
+*t == NULL condition will handle it.
+Similar for floor.
+*/
+
 typedef struct TreeNode* Tree;
 struct TreeNode {
 	int val;
@@ -25,6 +40,32 @@ void insert(Tree *t, int val) {
 	else insert(&((*t)->right), val);
 }
 
+/* Return smallest element >= k in tree (CEILING). */
+int searchCeil(Tree *t, int k) {
+	if (*t == NULL) {
+		return INT_MIN;
+	}
+	if (k == (*t)->val) return k;
+	if (k > (*t)->val) {
+		return searchCeil(&((*t)->right), k);
+	}
+	int possible = searchCeil(&((*t)->left), k);
+	return possible >= k? possible: (*t)->val;
+}
+
+/* Return largest element <= k in tree (FLOOR). */
+int searchFloor(Tree *t, int k) {
+	if (*t == NULL) {
+		return INT_MAX;
+	}
+	if (k == (*t)->val) return k;
+	if (k < (*t)->val) {
+		return searchFloor(&((*t)->left), k);
+	}
+	int possible = searchFloor(&((*t)->right), k);
+	return possible <= k? possible: (*t)->val;
+}
+
 void print(Tree t) {
 	if (t == NULL) return;
 	print(t->left);
@@ -41,5 +82,6 @@ int main(int argc, char **argv) {
 	insert(&tree,7);
 	print(tree);
 	std::cout << "\n";
+	std::cout << searchFloor(&tree, 8) << " " << searchCeil(&tree, 8) << '\n';
 	return 0;
 }
