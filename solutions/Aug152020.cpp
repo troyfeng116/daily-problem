79e83b4a97f5b2a393184d2258ab0413b4089041
Troy Feng <troy.feng@yale.edu>
2020-08-15 14:06:35 -0400

    Solved 8/15: #ways to climb stairs recursively and in linear time

---
+++ solutions/Aug152020.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/*
+You are given a positive integer N which represents the number of steps in a staircase. You can either 
+climb 1 or 2 steps at a time. Write a function that returns the number of unique ways to climb the stairs.
+
+Can you find a solution in O(n) time?
+*/
+
+/*
+Let f(n) denote the number of ways to climb n stairs.
+f(0) = 1
+f(1) = 1
+f(2) = 2
+f(3) = f(3-1)+f(3-2) = 3
+f(4) = f(4-1)+f(4-2) = 5
+...
+f(n) = f(n-1)+f(n-2)
+We can find the n'th Fibonacci number in exponential time recursively, linear time using memoization or
+Markov chain, and constat time using Binet's.
+*/
+
+int numWaysRecursive(int n) {
+	if (n < 0) return 0;
+	if (n <= 1) return 1;
+	return numWaysRecursive(n-1) + numWaysRecursive(n-2);
+}
+
+int numWaysLinear(int n) {
+	if (n <= 1) return 1;
+	int n2 = 1; // f(k-2)
+	int n1 = 1; // f(k-1)
+	int k = 2; // k
+	while (k <= n) {
+		n1 = n1+n2;
+		n2 = n1-n2;
+		k++;
+	}
+	return n1;
+}
+
+int main(int argc, char **argv) {
+	std::cout << numWaysRecursive(8) << '\n';
+	std::cout << numWaysLinear(8) << '\n';
+	return 0;
+}
