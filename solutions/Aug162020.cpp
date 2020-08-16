c7f49e99a4540098a544f4ee62d973c41a81295d
Troy Feng <troy.feng@yale.edu>
2020-08-16 13:46:19 -0400

    Solved Pythag triple in O(N^2) time using set memoization

--- solutions/Aug162020.cpp
+++ solutions/Aug162020.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
 using namespace std;
 
 /*
@@ -12,6 +15,41 @@ Output: True
 Here, 5^2 + 12^2 = 13^2.
 */
 
+/* 
+QUESTION: 
+-does the triple have to be ordered in the list (i.e. a subsequence)? I will assume no.
+-are the numbers in the list guaranteed to be integers? I will assume yes (Pythagorean triples are defined
+to be integers)
+*/
+
+/*
+We can take O(N) to add the squares of each element in the list to a set. Then, since all integer Pythagorean
+triples have no two sides equal, we can just loop over all (a,b) pairs and look up if a^2 + b^2 is in the set.
+Overall runtime: O(N^2).
+*/
+bool hasPythagTriple(vector<int> list) {
+	set<int> table;
+	for (int i = 0; i < list.size(); i++) table.insert(list[i]*list[i]);
+	for (int i = 0; i < list.size(); i++) {
+		for (int j = i+1; j < list.size(); j++) {
+			int a = list[i];
+			int b = list[j];
+			if (table.count(a*a+b*b) > 0) {
+				std::cout <<"FOUND: "<<a<<' '<<b<<' '<<sqrt(a*a+b*b)<<'\n';
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 int main(int argc, char **argv) {
+	vector<int> list;
+	list.push_back(12);
+	list.push_back(5);
+	list.push_back(12);
+	list.push_back(5);
+	list.push_back(13);
+	std::cout << hasPythagTriple(list) << '\n';
 	return 0;
 }
