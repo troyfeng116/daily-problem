d6f3b06c920d00c772961115e4e7b730c629a38a
Troy Feng <troy.feng@yale.edu>
2020-08-16 13:33:51 -0400

    Created cpp file for 8/16

---
+++ solutions/Aug162020.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+Given a list of numbers, find if there exists a pythagorean triplet in that list. A pythagorean triplet 
+is 3 variables a, b, c where a^2 + b^2 = c^2
+
+Example:
+Input: [3, 5, 12, 5, 13]
+Output: True
+
+Here, 5^2 + 12^2 = 13^2.
+*/
+
+int main(int argc, char **argv) {
+	return 0;
+}
