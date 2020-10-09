3e7bfa83dbf09448ccf2e1666af256b1345f1390
Troy Feng <troy.feng@yale.edu>
2020-10-09 10:25:00 -0400

    Solved 10/8: stack with getMin() in constant space

--- solutions/Oct2020/Oct082020.cpp
+++ solutions/Oct2020/Oct082020.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 /*
@@ -12,7 +13,7 @@ getMin() -- Retrieve the minimum element in the stack.
 Note: be sure that pop() and top() handle being called on an empty stack.
 */
 
-/* APPROACH 1: Add another field to stack. O(1) getMin but O(N) space */
+/* APPROACH 1: Add another field to each stacknode (i.e. custom stack). O(1) getMin but O(N) space */
 typedef struct StackNode1 *Stack1;
 struct StackNode1 {
     int val;
@@ -65,7 +66,74 @@ void printStack(Stack1 *s) {
     std::cout << '\n';
 }
 
+/* APPROACH 2: O(1) space. Store min in one variable.
+-push: if val < min, we can store 2*val-min in the stack and set min=val.
+-pop: if top < min, update min = 2*min-top.  */
+struct Stack2 {
+    int min;
+    stack<int> s;
+    
+    void push(int val) {
+        if (s.empty()) {
+            min = val;
+            s.push(val);
+        }
+        else {
+            if (val >= min) s.push(val);
+            else {
+                s.push(2*val - min);
+                min = val;
+            }
+        }
+    }
+
+    int top() {
+        if (s.empty()) {
+            std::cout << "ERROR: TRIED TO TOP EMPTY STACK\n";
+            return -1;
+        }
+        if (s.top() < min) {
+            return 2*min - s.top();
+        }
+        return s.top();
+    }
+
+    int pop() {
+        if (s.empty()) {
+            std::cout << "ERROR: TRIED TO POP EMPTY STACK\n";
+            return -1;
+        }
+        int t = s.top();
+        s.pop();
+        if (t < min) {
+            int ans = min;
+            min = 2*min - t;
+            return ans;
+        }
+        return t;
+    }
+
+    int getMin() {
+        if (s.empty()) {
+            std::cout << "ERROR: TRIED TO GET MIN FROM EMPTY STACK\n";
+            return -1;
+        }
+        return min;
+    }
+
+    void print() {
+        stack<int> copy(s);
+        while (!copy.empty()) {
+            std::cout << copy.top() << ' ';
+            copy.pop();
+        }
+        std::cout << '\n';
+    }
+};
+
 int main(int argc, char **argv) {
+    // APPROACH 1 TESTING
+    std::cout << "APPROACH 1 TESTING\n";
     Stack1 stack = NULL;
     push(&stack, -2);
     push(&stack, 0);
@@ -75,5 +143,23 @@ int main(int argc, char **argv) {
     std::cout << top(&stack) << '\n';
     std::cout << getMin(&stack) << '\n';
     printStack(&stack);
+    std::cout << '\n';
+
+    // APPROACH 2 TESTING
+    std::cout << "APPROACH 2 TESTING\n";
+    Stack2 stak;
+    stak.push(-2);
+    stak.push(0);
+    stak.push(3);
+    std::cout << stak.getMin() << '\n';
+    std::cout << stak.pop() << '\n';
+    std::cout << stak.top() << '\n';
+    std::cout << stak.getMin() << '\n';
+    std::cout << stak.pop() << '\n';
+    std::cout << stak.pop() << '\n';
+    std::cout << stak.pop() << '\n';
+    std::cout << stak.pop() << '\n';
+    stak.print();
+    std::cout << '\n';
     return 0;
 }
